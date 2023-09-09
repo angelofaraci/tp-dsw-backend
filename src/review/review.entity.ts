5344f06148e9dd4f2bd3d29404b9e54a9c3fa304
angelofaraci <angelofaraci089@yahoo.com>
2023-09-09 19:58:58 -0300

    creacion de review

---
+++ src/review/review.entity.ts
@@ -0,0 +1,14 @@
+import crypto from 'node:crypto'
+
+export class Review {
+    constructor(
+        
+        public rating: number,
+        public body: string,
+        public spoiler_check: boolean,
+        public redaction_date: Date,
+        public edition_date: Date | null,
+        public state: string,
+        public id = crypto.randomUUID()
+    ){}
+}
---
+++ src/review/review.repository.ts
@@ -0,0 +1,15 @@
+import { Review } from "./review.entity.js";
+
+const reviews = [
+   new Review (
+    83,
+    'lasjdklaskldj',
+    false,
+    new Date(2023, 0o6, 0o6),
+    null,
+    'jkasldjlaskjdla'
+   )
+];
+
+
+
