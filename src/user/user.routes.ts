31c1512d2942551ad8e2ab6887326ae7bda80eb3
angelofaraci <angelofaraci089@yahoo.com>
2023-09-17 20:18:01 -0300

    poco cambio

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { sanitizeUserInput, add, getOne, verifyToken } from './user.controller.js';
+import { sanitizeUserInput, add, getOne, verifyToken, getUserData } from './user.controller.js';
 
 export const userRouter = Router()
 
 userRouter.post('/signup', sanitizeUserInput, add)
 userRouter.post('/login', sanitizeUserInput, getOne)
+userRouter.get('/:id', verifyToken, getUserData)
