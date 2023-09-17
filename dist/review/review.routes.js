d50cb8bdf7dc70b160e1efe7066de61bfe9b05b4
angelofaraci <angelofaraci089@yahoo.com>
2023-09-16 22:28:10 -0300

    Eliminacion de dist

--- dist/review/review.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { sanitizeReviewInput, findAll, findOne, add, update, remove } from './review.controller.js';
-export const reviewRouter = Router();
-reviewRouter.get('/', findAll);
-reviewRouter.get('/:id', findOne);
-reviewRouter.post('/', sanitizeReviewInput, add);
-reviewRouter.put('/:id', sanitizeReviewInput, update);
-reviewRouter.delete('/:id', remove);
-//# sourceMappingURL=review.routes.js.map
