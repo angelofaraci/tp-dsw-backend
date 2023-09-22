82a76a41e37a429df03cce35252f9ec03accabad
angelofaraci <angelofaraci089@yahoo.com>
2023-09-22 16:38:36 -0300

    Manejo de login con try/catch

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,6 +2,7 @@ import { User } from "./user.entity.js"
 import jwt from 'jsonwebtoken';
 
 export class UserRepository {
+    
     public async add(item:any):Promise<string | undefined>{
         const newUser = new User(item)
         if (await User.findOne({email: item.email})){
@@ -11,13 +12,23 @@ export class UserRepository {
         return jwt.sign({_id: newUser._id}, 'secretKey')
     }
 
-    public async getOne(email:string, password:string ): Promise<string| number | undefined>{
-        const userLogIn = await User.findOne({email: email})
-        if (!userLogIn) return 1
-        if (userLogIn.password !== password) return 2   
-        return jwt.sign({_id: userLogIn._id}, 'secretKey')
+
+//Busca usuario por email y devuelve token
+    public async getOne(email:string, password:string ): Promise<string| number | undefined | unknown>{
+        try{
+            const userLogIn = await User.findOne({email: email}) || undefined
+            if (!userLogIn) throw new Error('Wrong Email')
+            if (userLogIn.password !== password) throw new Error('Wrong Password')
+            return jwt.sign({_id: userLogIn._id}, 'secretKey')
+        } catch(error: any){
+             return error.message
+        }
+        
+        
     }
 
+    
+//Busca usuario por _id y devuelve sus datos
     public async recoverOne(id:string): Promise<{}| null | undefined>{
         const user = await User.findById(id) 
         if (user) return user
