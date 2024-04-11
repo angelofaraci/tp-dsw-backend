6595c3e5359edbdd87215622d6c3d790d9dcff2a
angelofaraci <angelofaraci089@yahoo.com>
2024-04-10 21:15:02 -0300

    metodo para obtener todos los usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,135 +1,181 @@
-import { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken';
-import { User, UserModel } from "./user.entity.js"
-import { Types } from 'mongoose';
-import { ReviewModel } from '../review/review.entity.js';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { User, UserModel } from "./user.entity.js";
+import { Types } from "mongoose";
+import { ReviewModel } from "../review/review.entity.js";
 
-const repository = UserModel
-
+const repository = UserModel;
 
 //verifies inputs
 
-function sanitizeUserInput(req:Request, res:Response, next: NextFunction ) {
-    req.body.sanitizedInput = {
-        id: req.body.id, //REVISAR!!!!!!!!!!!!!!
-        username : req.body.username,
-        score : req.body.score,
-        email : req.body.email,
-        password: req.body.password,
-        phone: req.body.phone,
-        level: req.body.level,
-    }
-    
-      next()
+function sanitizeUserInput(req: Request, res: Response, next: NextFunction) {
+  req.body.sanitizedInput = {
+    id: req.body.id, //REVISAR!!!!!!!!!!!!!!
+    username: req.body.username,
+    score: req.body.score,
+    email: req.body.email,
+    password: req.body.password,
+    phone: req.body.phone,
+    level: req.body.level,
+  };
 
-};
+  next();
+}
 
 //adds an User to the repository and returns the correspondant token
 
-async function add (req: Request, res: Response, next: NextFunction) {
- 
-    const input = req.body.sanitizedInput;
-    input.level = 1;
-    input.score = 0;
-    const user = await repository.findOne({$or: [{email: input.email}, {username: input.username}]});
-    console.log(user)
-    if (user){
-        if (user.email = input.email){
-            return res.status(400).send({message:'User with that email already exists'})
-        }
-        return res.status(400).send({message:'Username taken'})
+async function add(req: Request, res: Response, next: NextFunction) {
+  const input = req.body.sanitizedInput;
+  input.level = 1;
+  input.score = 0;
+  const user = await repository.findOne({
+    $or: [{ email: input.email }, { username: input.username }],
+  });
+  console.log(user);
+  if (user) {
+    if ((user.email = input.email)) {
+      return res
+        .status(400)
+        .send({ message: "User with that email already exists" });
     }
-    const newUser = new repository(input);
-    await newUser.save();
-    const token = jwt.sign({_id: newUser._id}, 'secretKey');
-    return res.status(201).json({token})
-};
+    return res.status(400).send({ message: "Username taken" });
+  }
+  const newUser = new repository(input);
+  await newUser.save();
+  const token = jwt.sign({ _id: newUser._id }, "secretKey");
+  return res.status(201).json({ token });
+}
 
 //sends email and password of an User and returns the correspondent token
 
-async function getOne(req: Request, res: Response){
-
-    const email = req.body.email;
-    const password  = req.body.password;
-    try{
-        const userLogIn = await repository.findOne({email: email}) || undefined;
-        if (!userLogIn) {throw new Error('This email does not correspond to any account')}
-        if (userLogIn.password !== password) {throw new Error('Wrong Password')}
-        const token = jwt.sign({_id: userLogIn._id}, 'secretKey');
-        return res.status(200).json({token})
-    } catch(error: any){
-        return res.status(401).send(error.message)
-    }   
-};
+async function getOne(req: Request, res: Response) {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const userLogIn = (await repository.findOne({ email: email })) || undefined;
+    if (!userLogIn) {
+      throw new Error("This email does not correspond to any account");
+    }
+    if (userLogIn.password !== password) {
+      throw new Error("Wrong Password");
+    }
+    const token = jwt.sign({ _id: userLogIn._id }, "secretKey");
+    return res.status(200).json({ token });
+  } catch (error: any) {
+    return res.status(401).send(error.message);
+  }
+}
 
 //sends an _id to the repository and returns the correspondent JSON object
 
-async function getUserData(req:Request, res:Response, next: NextFunction){
-    const userData = await repository.findById(res.locals.userId);
-    res.locals.user = userData;
-    return res.status(200).json({userData})
+async function getUserData(req: Request, res: Response, next: NextFunction) {
+  const userData = await repository.findById(res.locals.userId);
+  res.locals.user = userData;
+  return res.status(200).json({ userData });
+}
+
+async function getAllUsersPublicData(req: Request, res: Response) {
+    console.log('llega')
+  const input = await repository.find();
+  const users = input.map((user) => ({
+    username: user.username,
+    level: user.level,
+    email: user.email,
+    score: user.score,
+  }));
+  if (users){
+    console.log(users)
+    return res.status(200).json(users)
+  }
+  return res.status(401).send({message: 'There was an error'})
 }
 
 //change the username of an User
 
-async function changeUsername(req:Request, res:Response, next: NextFunction){
-    const username = req.body.username;
-    const exists = await repository.findOne({username:username});
-    if (exists) {return res.status(400).send({message:'This username is already taken'})}
-    const id = res.locals.userId;
-    const user = await repository.findOneAndUpdate({_id:id}, {username:username}, {new: true});
-    if (user){
-        return res.status(200).json({user})
-    }
-    return res.status(401).send({message:'User not found'})
+async function changeUsername(req: Request, res: Response, next: NextFunction) {
+  const username = req.body.username;
+  const exists = await repository.findOne({ username: username });
+  if (exists) {
+    return res.status(400).send({ message: "This username is already taken" });
+  }
+  const id = res.locals.userId;
+  const user = await repository.findOneAndUpdate(
+    { _id: id },
+    { username: username },
+    { new: true }
+  );
+  if (user) {
+    return res.status(200).json({ user });
+  }
+  return res.status(401).send({ message: "User not found" });
 }
 
 //change the level of an User
 
-async function changeLevel(req:Request, res:Response, next:NextFunction) {
-    const input = req.body.sanitizedInput;
-    const user = await repository.findOneAndUpdate({email:input.email}, {level:input.level}, {new: true});
-    if (user){
-        return res.status(200).json({user})
-    }
-    return res.status(401).send({message:'User not found'})
+async function changeLevel(req: Request, res: Response, next: NextFunction) {
+  const input = req.body.sanitizedInput;
+  const user = await repository.findOneAndUpdate(
+    { email: input.email },
+    { level: input.level },
+    { new: true }
+  );
+  if (user) {
+    return res.status(200).json({ user });
+  }
+  return res.status(401).send({ message: "User not found" });
 }
 
-async function getOneUserPublicData(req: Request, res: Response){
-    const input = await repository.findOne({username: req.params.username})
-    const publicInput =  {_id: input?._id, email :input?.email ,username: req.params.username, level: input?.level, score: input?.score}
-    if (input){
-        return res.status(200).json({publicInput})
-    }
-    return res.status(401).send({message: 'User not found'})
+async function getOneUserPublicData(req: Request, res: Response) {
+  const input = await repository.findOne({ username: req.params.username });
+  const publicInput = {
+    _id: input?._id,
+    email: input?.email,
+    username: req.params.username,
+    level: input?.level,
+    score: input?.score,
+  };
+  if (input) {
+    return res.status(200).json({ publicInput });
+  }
+  return res.status(401).send({ message: "User not found" });
 }
 
 //verifies token validity
 
- function verifyToken(req: Request, res: Response, next: NextFunction) {
-    if (!req.headers.authorization){
-        return res.status(401).send('Unauthorized request')
-    }
-    const token = req.headers.authorization.split(' ')[1];
-    if (token === null){
-        return res.status(401).send('Unauthorized request')
-    }
-    const payload: any = jwt.verify(token, 'secretKey');
-    res.locals.userId = payload._id;
-    next()
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Unauthorized request");
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (token === null) {
+    return res.status(401).send("Unauthorized request");
+  }
+  const payload: any = jwt.verify(token, "secretKey");
+  res.locals.userId = payload._id;
+  next();
 }
 
 //delete an User by its id
 
-async function deleteUser(req: Request, res: Response ){
-    const id: Types.ObjectId = new Types.ObjectId(req.params.id);
-    await ReviewModel.deleteMany({userId: id});
-    const result = await repository.findByIdAndDelete(id);
-    if (result){
-        return res.status(204).send('User deleted succesfully')
-    }
-    return res.status(401).send('Something went wrong')
+async function deleteUser(req: Request, res: Response) {
+  const id: Types.ObjectId = new Types.ObjectId(req.params.id);
+  await ReviewModel.deleteMany({ userId: id });
+  const result = await repository.findByIdAndDelete(id);
+  if (result) {
+    return res.status(204).send("User deleted succesfully");
+  }
+  return res.status(401).send("Something went wrong");
 }
 
-export { deleteUser, sanitizeUserInput, add, getOne, verifyToken, getUserData , changeLevel, changeUsername, getOneUserPublicData }
-
+export {
+  deleteUser,
+  sanitizeUserInput,
+  add,
+  getOne,
+  verifyToken,
+  getUserData,
+  changeLevel,
+  changeUsername,
+  getOneUserPublicData,
+  getAllUsersPublicData
+};
--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -1,12 +1,24 @@
-import { Router } from 'express';
-import { sanitizeUserInput, add, getOne, verifyToken, getUserData, changeLevel, changeUsername, deleteUser, getOneUserPublicData } from './user.controller.js';
+import { Router } from "express";
+import {
+  sanitizeUserInput,
+  add,
+  getOne,
+  verifyToken,
+  getUserData,
+  changeLevel,
+  changeUsername,
+  deleteUser,
+  getOneUserPublicData,
+  getAllUsersPublicData
+} from "./user.controller.js";
 
 export const userRouter = Router();
 
-userRouter.post('/signup', sanitizeUserInput, add);
-userRouter.post('/login', sanitizeUserInput, getOne);
-userRouter.get('/profile', verifyToken, getUserData);
-userRouter.get('/user/:username', getOneUserPublicData)
-userRouter.put('/levelUp', sanitizeUserInput, changeLevel);
-userRouter.put('/update', sanitizeUserInput, verifyToken, changeUsername);
-userRouter.delete('/:id', deleteUser);
+userRouter.post("/signup", sanitizeUserInput, add);
+userRouter.post("/login", sanitizeUserInput, getOne);
+userRouter.get("/profile", verifyToken, getUserData);
+userRouter.get("/user/:username", getOneUserPublicData);
+userRouter.get("/getall", getAllUsersPublicData)
+userRouter.put("/levelUp", sanitizeUserInput, changeLevel);
+userRouter.put("/update", sanitizeUserInput, verifyToken, changeUsername);
+userRouter.delete("/:id", deleteUser);
